02af86c7cdb06064fa4534aa8762677b417f58b9
djpowehi <vicenzotulio@gmail.com>
2026-05-11 22:07:37 -0300

    chore: untrack internal dev artifacts (mcp config, scratch, anchor reference, day-1 planning, migrations)

--- programs/seedling/src/state/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod family_position;
-pub mod vault_config;
-
-pub use family_position::*;
-pub use vault_config::*;
--- programs/seedling/src/utils/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod harvest;
-pub mod shares;
-
-pub use harvest::*;
-pub use shares::*;
